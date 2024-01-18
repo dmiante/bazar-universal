5a754313cbbef226c2003acd4cfeb6b4cec0b642
dmiante <dmi4ndev@gmail.com>
2024-01-18 01:42:11 -0300

    add: card component

---
+++ src/app/store/components/list-products/list-products.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, type OnInit } from '@angular/core';
+import { Product } from '../../interfaces/product.interface';
+import { SearchListComponent } from '../../pages/search-list/search-list.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'card-list-products',
+  standalone: true,
+  imports: [CommonModule, SearchListComponent,RouterLink, RouterOutlet],
+  templateUrl: './list-products.component.html',
+})
+export class ListProductsComponent implements OnInit {
+  @Input()
+  public product!: Product;
+
+  ngOnInit(): void {
+    if (!this.product) throw Error('Product property is required');
+  }
+}
