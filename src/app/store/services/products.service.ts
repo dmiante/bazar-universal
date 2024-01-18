44c29b6e7f96c8e8ccdfb505ef1fdc71cbdf10d9
dmiante <dmi4ndev@gmail.com>
2024-01-18 18:37:08 -0300

    add: functions to fetch products by query and id

--- src/app/store/services/products.service.ts
+++ src/app/store/services/products.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { env } from 'src/env/env';
 import { Product } from '../interfaces/product.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,8 +12,15 @@ export class ProductsService {
 
   constructor(private http: HttpClient) {}
 
-  getProducts() {
+  getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(`${this.baseURL}/products`);
   }
 
+  getProductsBySearch(query: string) {
+    return this.http.get<Product[]>(`${this.baseURL}/items?q=${query}`);
+  }
+
+  getProductById(id: string): Observable<Product|undefined> {
+    return this.http.get<Product>(`${this.baseURL}/products/${id}`);
+  }
 }
