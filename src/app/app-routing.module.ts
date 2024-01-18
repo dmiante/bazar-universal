3bd177c2de2f850143586919f759af751c0f57b9
dmiante <dmi4ndev@gmail.com>
2024-01-18 18:34:31 -0300

    fix: routing and redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SearchBarComponent } from './store/pages/search-bar/search-bar.component';
-import { SearchListComponent } from './store/pages/search-list/search-list.component';
-import { SearchDetailComponent } from './store/pages/search-detail/search-detail.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./store/store.module').then(m => m.StoreModule)
+    loadChildren: () =>
+      import('./store/store.module').then((m) => m.StoreModule),
   },
-  {
-    path: '**',
-    redirectTo: 'products'
-  }
+  // {
+  //   path: '**',
+  //   redirectTo: '',
+  // }
 ];
 
 @NgModule({
--- src/app/store/store-routing.module.ts
+++ src/app/store/store-routing.module.ts
@@ -3,19 +3,30 @@ import { RouterModule, Routes } from "@angular/router";
 import { SearchBarComponent } from "./pages/search-bar/search-bar.component";
 import { SearchListComponent } from "./pages/search-list/search-list.component";
 import { SearchDetailComponent } from "./pages/search-detail/search-detail.component";
+import { LayoutPageComponent } from "./pages/layout-page/layout-page.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: SearchBarComponent,
-  },
-  {
-    path: 'items',
-    component: SearchListComponent,
-  },
-  {
-    path: 'items/:id',
-    component: SearchDetailComponent,
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: '',
+        component: SearchBarComponent,
+      },
+      {
+        path: 'items',
+        component: SearchListComponent,
+      },
+      {
+        path: 'items/:id',
+        component: SearchDetailComponent,
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      },
+    ],
   },
 ];
 
